a20a6d3b691c6958b56ee217fd5bc4b0aa1bc8b5
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2019-03-20 12:21:40 +0000

    Fix swagger specification for Alfresco MediaType
    Reuse of common swagger configuration defined in common repository.
    
    Refs https://github.com/Activiti/Activiti/issues/2588

--- activiti-cloud-services-org/activiti-cloud-services-org-rest/src/main/java/org/activiti/cloud/services/organization/swagger/SwaggerConfiguration.java
+++ activiti-cloud-services-org/activiti-cloud-services-org-rest/src/main/java/org/activiti/cloud/services/organization/swagger/SwaggerConfiguration.java
@@ -16,47 +16,16 @@
 
 package org.activiti.cloud.services.organization.swagger;
 
-import org.activiti.cloud.alfresco.rest.model.ListResponseContent;
-import org.activiti.cloud.organization.api.Project;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger configuration
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean(Docket.class)
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("org.activiti.cloud.services.organization.rest.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    @Bean
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Activiti Modeling REST API")
-                .description("Provides access to the core features of Activiti Modeling.")
-                .version("1.0")
-                .build();
-    }
 
     @Bean
     public UiConfiguration uiConfig() {
@@ -65,11 +34,4 @@ public class SwaggerConfiguration {
                 .build();
     }
 
-    /**
-     * This is a shim class that serves to generate a meaningful swagger spec
-     * See: https://github.com/swagger-api/swagger-core/issues/498
-     */
-    public class AlfrescoProjectsPage extends ListResponseContent<Project> {
-
-    }
 }
